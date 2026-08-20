e7304d60c05ad508be4fa4bf2814f9b09414ed16
thennarasumurugan <thennu543@gmail.com>
2026-08-20 14:17:00 +0530

    RDBMS PROGRAM-1 1U25IT110

--- solution.sql
+++ solution.sql
@@ -1,1 +1,8 @@
-
+CREATE DATABASE universityDB
+USE universityDB
+CREATE TABLE Department(
+  DepartmentID numeric(5)PRIMARY KEY,
+  DepartmentName varchar(20)NOT NULL,
+  HOD varchar(20)NOT NULL
+  );
+    desc Department
